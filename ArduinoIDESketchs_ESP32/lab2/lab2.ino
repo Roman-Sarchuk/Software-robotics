5718a9e59abbe609e3b0be923d591f6096fefb4b
Roman-Sarchuk <91730539+Roman-Sarchuk@users.noreply.github.com>
2026-03-05 21:57:44 +0200

    Refactor LED and LCD handling, implement sound feedback for distance measurement

--- ArduinoIDESketchs_ESP32/lab2/lab2.ino
+++ ArduinoIDESketchs_ESP32/lab2/lab2.ino
@@ -1,23 +1,42 @@
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 
-// LED pins (безпечні GPIO)
-const int greenPin  = 18;
-const int yellowPin = 19;
-const int redPin    = 23;
+// ----- LED pins -----
+const int greenPin  = 16;
+const int yellowPin = 17;
+const int redPin    = 18;
 
-// Piezo pin
+// ----- LCD (I2C) -----
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+// ----- Piezo pin -----
 const int piezoPin = 27;
 
-// HC-SR04 pins
+// ----- HC-SR04 pins -----
 const int trigPin = 26;
 const int echoPin = 25;
 
-// LCD: I2C address (може бути 0x27 або 0x3F)
-LiquidCrystal_I2C lcd(0x27, 16, 2);
+// ----- Buttons -----
+const int powerButton = 33;
+
+// ----- params -----
+enum SoundType 
+{
+  GREEN,
+  YELLOW,
+  RED,
+  CRITICAL
+};
+
+volatile bool isSystemEnabled = true;
+unsigned long lastInterrupt = 0;
+
+bool isDistanceTextDisplayed = false;
+bool isSystemOffTextDisplayed = false;
 
 float dist = 0;
 
+// ----- My functions -----
 long readUltrasonicDistance(int trig, int echo) {
   digitalWrite(trig, LOW);
   delayMicroseconds(2);
@@ -30,7 +49,48 @@ long readUltrasonicDistance(int trig, int echo) {
   return duration;
 }
 
+void makeSound(SoundType soundType) {
+  switch (soundType)
+  {
+    case GREEN:
+      tone(piezoPin, 150, 200);
+      break;
+
+    case YELLOW:
+      tone(piezoPin, 300, 200);
+      break;
+
+    case RED:
+      tone(piezoPin, 400, 200);
+      break;
+
+    case CRITICAL:
+      tone(piezoPin, 523, 200);
+      break;
+  }
+}
+
+void clearLCD(int rowIndex) {
+  lcd.setCursor(0, rowIndex);
+  lcd.print("                ");
+}
+
+
+// ----- intrerrapt function -----
+void IRAM_ATTR handleButtonInterrupt()
+{
+  unsigned long now = millis();
+
+  if (now - lastInterrupt > 200)
+  {
+    isSystemEnabled = !isSystemEnabled;
+    lastInterrupt = now;
+  }
+}
+
+// ----- Core functions -----
 void setup() {
+  // Pins
   pinMode(greenPin, OUTPUT);
   pinMode(yellowPin, OUTPUT);
   pinMode(redPin, OUTPUT);
@@ -45,40 +105,78 @@ void setup() {
   lcd.clear();
   lcd.setCursor(0, 0);
   lcd.print("Distance:");
+
+  // Interrupt
+  pinMode(powerButton, INPUT_PULLUP);
+
+  attachInterrupt(
+    digitalPinToInterrupt(powerButton),
+    handleButtonInterrupt,
+    FALLING
+  );
 }
 
 void loop() {
-  dist = 0.01723 * readUltrasonicDistance(trigPin, echoPin); // cm
+  if (isSystemEnabled)
+  {
+    // get value
+    dist = 0.01723 * readUltrasonicDistance(trigPin, echoPin); // cm
 
-  // Вивід на LCD
-  lcd.setCursor(0, 1);
-  lcd.print("        "); // очищаємо попереднє значення
-  lcd.setCursor(0, 1);
-  lcd.print(dist);
-  lcd.print(" cm");
+    // processed LCD
+    if (isSystemOffTextDisplayed)
+    {
+      clearLCD(0);
+      isSystemOffTextDisplayed = false;
+    }
+    if (!isDistanceTextDisplayed)
+    {
+      lcd.setCursor(0, 0);
+      lcd.print("Distance:");
+      isDistanceTextDisplayed = true;
+    }
+    clearLCD(1);
+    lcd.setCursor(0, 1);
+    lcd.print(dist);
+    lcd.print(" cm");
 
-  // LED + Piezo
-  if (dist > 200) {
-    digitalWrite(greenPin, HIGH);
-    digitalWrite(yellowPin, LOW);
-    digitalWrite(redPin, LOW);
-    tone(piezoPin, 200, 200);
-  } else if (dist > 150) {
-    digitalWrite(greenPin, LOW);
-    digitalWrite(yellowPin, HIGH);
-    digitalWrite(redPin, LOW);
-    tone(piezoPin, 300, 200);
-  } else if (dist > 100) {
-    digitalWrite(greenPin, LOW);
-    digitalWrite(yellowPin, LOW);
-    digitalWrite(redPin, HIGH);
-    tone(piezoPin, 400, 200);
-  } else {
-    digitalWrite(greenPin, LOW);
-    digitalWrite(yellowPin, LOW);
-    digitalWrite(redPin, HIGH);
-    tone(piezoPin, 523, 200);
+    // LED + Piezo
+    if (dist > 300) {
+      digitalWrite(greenPin, HIGH);
+      digitalWrite(yellowPin, LOW);
+      digitalWrite(redPin, LOW);
+      makeSound(GREEN);
+      
+    } else if (dist > 200) {
+      digitalWrite(greenPin, LOW);
+      digitalWrite(yellowPin, HIGH);
+      digitalWrite(redPin, LOW);
+      makeSound(YELLOW);
+    } else if (dist > 100) {
+      digitalWrite(greenPin, LOW);
+      digitalWrite(yellowPin, LOW);
+      digitalWrite(redPin, HIGH);
+      makeSound(RED);
+    } else {
+      digitalWrite(greenPin, LOW);
+      digitalWrite(yellowPin, LOW);
+      digitalWrite(redPin, HIGH);
+      makeSound(CRITICAL);
+    }
+
   }
-
-  delay(200); // невелика затримка для стабільності
+  else {
+    if (isDistanceTextDisplayed)
+    {
+      clearLCD(0);
+      clearLCD(1);
+      isDistanceTextDisplayed = false;
+    }
+    if (!isSystemOffTextDisplayed)
+    {
+      lcd.setCursor(0, 0);
+      lcd.print("System OFF");
+      isSystemOffTextDisplayed = true;
+    }
+  }
+  delay(200);
 }
